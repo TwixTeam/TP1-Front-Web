54b23cbb69be60c1f4ab3de7ecd13e3f22bf40dd
Bilal <bilal.abbad@isep.fr>
2017-12-10 04:36:37 +0100

    setup linter + formatter + axios + cleaner code

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-const path = require('path');
+const path = require('path')
 
-const SRC_DIR = path.resolve(__dirname, "site");
+const SRC_DIR = path.resolve(__dirname, 'site')
 
 module.exports = {
     entry: [
-        SRC_DIR + "/js/index.js"
+        SRC_DIR + '/js/index.js'
     ],
     devtool: 'eval-source-map',
     module: {
-        loaders: [
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js?/,
+                exclude: /node_modules/,
+                use: ['eslint-loader']
+            },
             {
                 test: /\.js?/,
                 exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
-                    presets: ["es2015"]
-                }
+                use: ['babel-loader']
             }
         ]
     },
     output: {
-        path: SRC_DIR + "/build",
-        filename: "app.js",
-        publicPath: "/build/"
+        path: SRC_DIR + '/build',
+        filename: 'app.js',
+        publicPath: '/build/'
     }
 }
